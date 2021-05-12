7953d578f9b9ab0216541884e6af0e2bb4c995bc
bakjonghun <bakjonghun@hikick.kr>
2021-05-13 04:11:59 +0900

    change: documentdb to self hosted mongodb

--- src/tools/mongodb.ts
+++ src/tools/mongodb.ts
@@ -4,17 +4,13 @@ import { readFileSync } from 'fs';
 
 export class MongoDB {
   public static async init(): Promise<void> {
-    const sslCA = [readFileSync('rds-combined-ca-bundle.pem')];
-    const MONGODB_URI =
-      process.env.MONGODB_URI || 'mongodb://localhost:27017/kickboard';
-    const hasSSL = MONGODB_URI.includes('ssl=true');
+    const DATABASE_URL =
+      process.env.DATABASE_URL || 'mongodb://localhost:27017/kickboard';
     mongoose.Promise = global.Promise;
-    await mongoose.connect(MONGODB_URI, {
+    await mongoose.connect(DATABASE_URL, {
       useCreateIndex: true,
       useUnifiedTopology: true,
       useNewUrlParser: true,
-      sslValidate: hasSSL ? false : undefined,
-      sslCA: hasSSL ? sslCA : undefined,
     });
 
     logger.info('[MongoDB] 데이터베이스 준비가 완료되었습니다.');
